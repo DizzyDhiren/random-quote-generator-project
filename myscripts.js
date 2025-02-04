2a2ae447ea879b82dc1ee0711c0f5b5f94bf9697
DizzyDhiren <dhiren007patel@gmail.com>
2025-02-04 11:04:16 +0000

    Generate quote function working

--- myscripts.js
+++ myscripts.js
@@ -1,16 +1,17 @@
-const HTML = document.getElement('HTML');
+const HTML = document.getElementById('HTML');
 const generateBtn = document.getElementById('generate-button');
 const quote = document.getElementById('quote'); 
 
 const quotesArray = [
-    {"to be or not to be"},
-    {"2"},
-    {"3"},
-    {"4"}
-]
+    "to be or not to be",
+    "2",
+    "3",
+    "4"
+];
 
 function generateQuote(){
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    
+    const randomIndex = Math.floor(Math.random() * quotesArray.length);
+    quote.textContent = `"${quotesArray[randomIndex]}"`;
+    console.log(randomIndex);
 
 }
