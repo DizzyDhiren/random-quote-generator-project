b271e9784195e9505e76208fa535ce1e5de0e0b5
DizzyDhiren <dhiren007patel@gmail.com>
2025-02-04 01:29:19 +0000

    test array in JS added//need to work on function

---
+++ myscripts.js
@@ -0,0 +1,16 @@
+const HTML = document.getElement('HTML');
+const generateBtn = document.getElementById('generate-button');
+const quote = document.getElementById('quote'); 
+
+const quotesArray = [
+    {"to be or not to be"},
+    {"2"},
+    {"3"},
+    {"4"}
+]
+
+function generateQuote(){
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    
+
+}
