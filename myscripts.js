ef7f8e3fc94a19a2a8d445e29338099e58080d12
DizzyDhiren <dhiren007patel@gmail.com>
2025-02-04 11:07:29 +0000

    Changed the array to be complete

--- myscripts.js
+++ myscripts.js
@@ -3,15 +3,17 @@ const generateBtn = document.getElementById('generate-button');
 const quote = document.getElementById('quote'); 
 
 const quotesArray = [
-    "to be or not to be",
-    "2",
-    "3",
-    "4"
+    "Today will be a good day",
+    "Make those small habits",
+    "1% better than yesterday",
+    "Not everyday is a perfect day", 
+    "You will make it", 
+    "Keep working hard"
 ];
 
 function generateQuote(){
     const randomIndex = Math.floor(Math.random() * quotesArray.length);
-    quote.textContent = `"${quotesArray[randomIndex]}"`;
+    quote.textContent = `${quotesArray[randomIndex]}`;
     console.log(randomIndex);
 
 }
